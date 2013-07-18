57b312300057b485c3c9711c849ced48f3aac863
Bernardo Rufino <contato@bernardorufino.com.br>
2013-07-18 05:07:46 -0300

    First page, TOS page, PP page.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,43 @@
-console.log('This would be the main JS file.');
+function $(id) {
+	return document.getElementById(id);
+}
+
+ScreenshotManager = {
+
+	SOURCE_TEMPLATE: 'images/screenshots/ss?.png',
+	LENGTH: 5,
+	FIRST: 1,
+
+	update: function(updater) {
+		var image = $('screenshot');
+		var regex = this.SOURCE_TEMPLATE.replace('?', '(\\d+)');
+		var match = new RegExp(regex).exec(image.src);
+		if (!match) return null;
+		var index = this.FIRST + updater(parseInt(match[1]) - this.FIRST, this.LENGTH);
+		$('counter').innerHTML = index;
+		image.src = this.SOURCE_TEMPLATE.replace('?', index);
+	}, 
+
+	previousClick: function(e) {
+		ScreenshotManager.update(function(i, length){
+			return (0 < i && i <= length - 1) ? i - 1 : i;
+		});
+	},
+
+	nextClick: function(e) {
+		var self = ScreenshotManager;
+		self.update(function(i, length){
+			return (0 <= i && i < length - 1) ? i + 1 : i;
+		});
+	},
+
+	initialize: function() {
+		$("previous").addEventListener('click', this.previousClick, false);
+		$("next").addEventListener('click', this.nextClick, false);
+	}
+
+}
+
+window.onload = function() {
+	ScreenshotManager.initialize();
+}
